0c9620ac3a1cc4bbb233ff695b5641217c228df9
Hasibur Rahman <132805051+hasib-90@users.noreply.github.com>
2023-10-05 20:03:01 +0600

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".js-sticknav").waypoint(function(direction){
+
+		if (direction=="down") {
+			$("nav").addClass("sticky");
+		} 	
+		else{
+			$("nav").removeClass("sticky");
+		}
+	});
+});
